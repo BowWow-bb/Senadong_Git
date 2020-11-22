304a37411cd23a12e433ba640f9595a3ecee89c9
daeun <sung980524@naver.com>
2020-11-23 00:54:27 +0900

    Battle Scene이랑 닭 디자인 ㅋㅋ

---
+++ Assets/Scripts/Chicken/Chicken_Move.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chicken_Move : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //테스트용 
+        Vector3 moveVelocity = Vector3.left;
+        transform.position += moveVelocity * Time.deltaTime*0.5f;
+    }
+}
