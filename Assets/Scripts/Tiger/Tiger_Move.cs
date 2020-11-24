c4841e3b3d7d16b05a96c6c9aa3163890577f0f6
Seochangh2 <71918056+Seochangh2@users.noreply.github.com>
2020-11-24 16:56:08 +0900

    23
    23

--- Assets/Scripts/Tiger/Tiger_Move.cs
+++ Assets/Scripts/Tiger/Tiger_Move.cs
@@ -7,25 +7,30 @@ using UnityEngine;
 public class Tiger_Move : MonoBehaviour
 {
     int hungryTime =0; // 배고픔 재는 시간
-    int BasicTime = 0; 
-    bool hunger;
-    bool moving;
-    float move_length=0;
-    Vector2 move_vec;
-    Vector3 Start_Point;
+    int BasicTime = 0; // 기본 움직임 재는 시간
+    int playTime = 0; // 심심한 시간 재는 시간
+
+    public bool playing = false; // 노는중 인지
+    bool hunger; // 배고픈 상태인지 
+    bool moving; // 움직이고 있는 상태 인지
+    float move_length=0; // 얼마나 움직였는지의 벡터
+
+    Vector2 move_vec; // 움직일 방향벡터
+    Vector3 Start_Point; // 움직일때의 시작점
+
 
     // Start is called before the first frame update
     public bool Hungry()
     {
-        if (hunger)
+        if (hunger) // 배고픈 상태 일 때
         {
             return true;
         }
         else {
-            if ((hungryTime / 100)%100 == 0 && (hungryTime) > 0)
+            if ((hungryTime / 100)%100 == 0 && (hungryTime) > 0) // 일정시간마다 배고파짐
             {
                 hungryTime = 0;
-                Debug.Log("호랑이 배고파!");
+              //  Debug.Log("호랑이 배고파!");
                 hunger = true;
             }
             return true;
@@ -38,7 +43,21 @@ public class Tiger_Move : MonoBehaviour
     }
     public bool FollowMouse()
     {
-        return true;
+        if (playing) // 놀고 있는 상태
+        {
+            return true;
+        }
+        else
+        {
+
+            if (playTime % 400 == 0 && (playTime) > 0) // 일정 시간마다 심심해짐
+            {
+                playTime = 0;
+                Debug.Log("호랑이 심심해!");
+            }
+            return true;
+        }
+
     }
     public bool Eat()
     {
@@ -46,13 +65,24 @@ public class Tiger_Move : MonoBehaviour
     }
     public bool BasicMove()
     {
-        if (moving)
+        if (moving && !playing) // 노는중 아닐 때 , 움직이는 중
         {
-            gameObject.transform.position = new Vector3(Start_Point.x+ move_vec.x * move_length,
-                                                   Start_Point.y + move_vec.y * move_length, Start_Point.z);
+            float x = Start_Point.x + move_vec.x * move_length; // 시작점 + 방향벡터 * 거리
+            float y = Start_Point.y + move_vec.y * move_length; 
+           
+            if (x >= 2.5f) // 울타리를 넘어가지 않기 위해 
+                x = 2.5f;
+            if (x <= -13f)
+                x = -13f;
+            if (y >= -4.5f)
+                y = -4.5f;
+            if (y <= -9f)
+                y = -9f;
 
-            move_length += 0.1f;
-            if (move_length > 10f)
+            gameObject.transform.position = new Vector3(x,y, Start_Point.z); // 이동
+
+            move_length += 0.05f; // 거리를 차근차근 움직임
+            if (move_length > 10f) // 다 움직였다면 다시 움직임 타이머를 잼
             {
                 moving = false;
                 BasicTime = 0;
@@ -61,12 +91,16 @@ public class Tiger_Move : MonoBehaviour
         }
         else
         {
-            if (BasicTime % 55 == 0 && BasicTime > 0)
+            if (BasicTime % 55 == 0 && BasicTime > 0) // 일정시간마다 혼자 돌아다님
             {
                 BasicTime = 0;
                 moving = true;
-                Start_Point = gameObject.transform.position;
-                move_vec = new Vector2(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f));
+                Start_Point = gameObject.transform.position; // 시작점 저장
+                move_vec = new Vector2(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f)); // 상하좌우,대각 랜덤으로 정함
+                if (move_vec.x >= 0)
+                    gameObject.transform.localScale = new Vector3(-1, 1, 1); // 왼쪽으로 움직인다면 왼쪽을 봄
+                else
+                    gameObject.transform.localScale = new Vector3(1, 1, 1); // 오른쪽이라면 오른쪽을 봄
                 move_length = 0;
             }
             return true;
@@ -74,8 +108,9 @@ public class Tiger_Move : MonoBehaviour
     }
     void Start()
     {
-        hunger = false;
+        hunger = false; // 변수 초기화
         moving = false;
+        playing = false;
     }
 
     // Update is called once per frame
@@ -85,7 +120,8 @@ public class Tiger_Move : MonoBehaviour
     }
     private void FixedUpdate()
     {
-        hungryTime ++;
+        hungryTime ++; // 타이머 
         BasicTime++;
+        playTime++;
     }
 }
