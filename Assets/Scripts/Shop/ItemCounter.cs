c9d925b27eba85e3504fcfef60eb5bcf01401f7f
Seochangh2 <71918056+Seochangh2@users.noreply.github.com>
2020-12-01 11:20:46 +0900

    dd
    d

---
+++ Assets/Scripts/Shop/ItemCounter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemCounter 
+{
+
+    // Start is called before the first frame update
+   public static class Variables
+    {
+        public static int Egg_Num;
+        public static int Poop_Num;
+        public static int Play_Num;
+        public static int Hungry_Num;
+        public static int Milk_Num;
+    }
+}
---
+++ Assets/Scripts/Shop/ShopManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void Sell_Milk()
+    {
+        ItemCounter.Variables.Milk_Num--;
+    }
+    public void Buy_Hungry()
+    {
+        ItemCounter.Variables.Hungry_Num++;
+       
+    }
+    public void Buy_Poop()
+    {
+        ItemCounter.Variables.Poop_Num++;
+    }
+    public void Sell_egg()
+    {
+        ItemCounter.Variables.Egg_Num--;
+    }
+    public void Buy_Play()
+    {
+        ItemCounter.Variables.Play_Num++;
+        Debug.Log(ItemCounter.Variables.Play_Num);
+    }
+}
