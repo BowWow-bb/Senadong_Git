89948862f1ec13c85225e161794d8c3e571340b0
daeun <sung980524@naver.com>
2020-12-08 00:31:23 +0900

    배틀씬 넘기기까지 햇삼

--- Assets/Map/Battle.cs
+++ Assets/Map/Battle.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class Battle : MonoBehaviour
 {
+    GameObject cow;
+    GameObject tiger;
+    GameObject chicken;
     // Start is called before the first frame update
     void Start()
     {
-        
+        cow = GameObject.FindWithTag("cow");
+        tiger = GameObject.FindWithTag("tiger");
+        chicken = GameObject.FindWithTag("chicken");
     }
 
     // Update is called once per frame
     public void OnClick()
     {
+        cow.SetActive(false);
+        tiger.SetActive(false);
+        chicken.SetActive(false);
         SceneManager.LoadScene("Battle_Scene");
 
     }
