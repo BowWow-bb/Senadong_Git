9506492638c4f24afc13fb8fba650491ffdec978
daeun <sung980524@naver.com>
2020-12-08 00:22:34 +0900

    아이템 슬롯

--- Assets/Map/Farm.cs
+++ Assets/Map/Farm.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class Farm : MonoBehaviour
 {
+    GameObject ItemSlot;
+    Vector3 Pos;
     // Start is called before the first frame update
     void Start()
     {
-        
+        ItemSlot = GameObject.Find("ItemSlot");
+        Pos = ItemSlot.transform.position;//농장 가기 전 위치 
     }
 
     // Update is called once per frame
     public void OnClick()
     {
+        ItemSlot.transform.position = new Vector3(Pos.x - 10, Pos.y, Pos.z);
         SceneManager.LoadScene("Farm_Scene");
     }
 }
--- Assets/Map/Store.cs
+++ Assets/Map/Store.cs
@@ -4,16 +4,19 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class Store : MonoBehaviour
 {
+    GameObject ItemSlot;
+    Vector3 Pos;
     // Start is called before the first frame update
     void Start()
     {
-
+        ItemSlot = GameObject.Find("ItemSlot");
+        Pos = ItemSlot.transform.position;//상점 가기 전 위치 
     }
 
     // Update is called once per frame
     public void OnClick()
     {
+        ItemSlot.transform.position = new Vector3(Pos.x + 10, Pos.y, Pos.z);
         SceneManager.LoadScene("Store_Scene");
-
     }
 }
